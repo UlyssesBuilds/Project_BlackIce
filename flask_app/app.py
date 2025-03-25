b14d24ad63e283b63927e35fb974b8a9216fcdb0
UlyssesBuilds <Ulysses.Vazquez04@gmail.com>
2025-03-25 16:57:16 -0700

    market page for Front End

--- flask_app/app.py
+++ flask_app/app.py
@@ -57,5 +57,9 @@ migrate = Migrate(app, db)
 def index():
     return render_template("index.html")
 
+@app.route('/market')
+def market():
+    return render_template('market.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
